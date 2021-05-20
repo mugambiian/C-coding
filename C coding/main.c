ed1fdaff4a63b9f647e78e0b0443fd173b650ccd
mugambiian <ianmugambi@gmail.com>
2021-05-20 16:48:25 +0300

    Initial Commit

---
+++ C coding/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  C coding
+//
+//  Created by Mururu Mugambi on 20/05/2021.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
